af5ba6173579ed39ed437301e2ac803f10d99c65
tomrosso <trosso2024@chadwickschool.org>
2021-11-18 12:20:07 -0800

    Worked in class

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using WordGuesser;
 public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
+    public GameObject StartScreen;
+    public GameObject PlayScreen;
+    private WordGuesser.WordGame guessingGame;
 
 
     public void StartGame()
     {
-        this.Message.text = "Can you save the Snowman?";
-        this.StartButton.gameObject.SetActive(false);
+        this.guessingGame = new WordGuesser.WordGame("apple", 5);
+        Debug.Log(this.guessingGame.GetWord());
+        Debug.Log(this.guessingGame.GetFullWord());
+
+        this.StartScreen.gameObject.SetActive(false);
+        this.PlayScreen.gameObject.SetActive(true);
     }
-
-
+    public void OpenStartScreen()
+    {
+        this.StartScreen.gameObject.SetActive(true);
+        this.PlayScreen.gameObject.SetActive(false);
+    }
+    public void Start()
+    {
+        this.StartScreen.gameObject.SetActive(true);
+        this.PlayScreen.gameObject.SetActive(false);
+    }
 
 }
