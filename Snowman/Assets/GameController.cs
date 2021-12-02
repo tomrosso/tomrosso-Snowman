30711233a3f4a99230b3bf1042da8e1922ade4b9
tomrosso <trosso2024@chadwickschool.org>
2021-12-02 12:22:15 -0800

    Worked on Milestone 5

--- Snowman/Assets/GameController.cs
+++ Snowman/Assets/GameController.cs
@@ -6,12 +6,17 @@ public class GameController : MonoBehaviour
 {
     public UnityEngine.UI.Text Message;
     public UnityEngine.UI.Button StartButton;
-
-    public UnityEngine.UI.Text HiddenLetters;
+    public UnityEngine.UI.Button PlayAgain;
+    public UnityEngine.UI.Text CorrectWord;
     public GameObject StartScreen;
     public GameObject PlayScreen;
     private WordGuesser.WordGame guessingGame;
     public UnityEngine.UI.InputField PlayerGuess;
+    public UnityEngine.UI.Text HiddenLetters;
+    public UnityEngine.UI.Text LettersGuessed;
+    public UnityEngine.UI.Text NumberOfGuessesLeft;
+    public UnityEngine.UI.Text CheckGuess;
+    public UnityEngine.UI.Text GameOver;
 
 
     public void StartGame()
@@ -36,7 +41,37 @@ public class GameController : MonoBehaviour
     public void SubmitGuess()
     {
         Debug.Log(this.guessingGame.CheckGuess(this.PlayerGuess.text));
+        this.DisplayHiddenLetters();
+        this.DisplayLettersGuessed();
+        this.DisplayNumberOfGuessesLeft();
+        this.DisplayCheckGuess(PlayerGuess.text);
+        
         PlayerGuess.text = string.Empty;
 
     }
+    public void DisplayHiddenLetters()
+    {
+        HiddenLetters.text = this.guessingGame.GetWord();
+    }
+    public void DisplayLettersGuessed()
+    {
+        LettersGuessed.text = this.guessingGame.GetGuessedLetters();
+    }
+    public void DisplayNumberOfGuessesLeft()
+    {
+        NumberOfGuessesLeft.text = $"{this.guessingGame.GetGuessLimit() - this.guessingGame.GetIncorrectGuesses()}";
+    }
+    public void DisplayCheckGuess(string guess)
+    {
+        CheckGuess.text = "Your guess is: " + this.guessingGame.CheckGuess(guess);
+    }
+    public void DisplayGameOver()
+    {
+        GameOver.text = "You Lost";
+    }
+    public void DisplayCorrectWord();
+    {
+        CorrectWord.text = "apple";
+    }
+
 }
